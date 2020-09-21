a6790f338f91e9f380e38ba119e7fee405bbdc4e
mattis <mattis@localhost-live.home>
2020-09-21 20:49:46 +0400

    Changed --help to -h

--- install.sh
+++ install.sh
@@ -12,4 +12,4 @@ fi
 chmod +x epidump_manager.sh
 mv epidump_manager.sh /usr/bin/epidump_manager
 
-echo -e "Epidump manager installed type epidump_manager --help to know how to use it\n"
+echo -e "Epidump manager installed type epidump_manager -h to know how to use it\n"
